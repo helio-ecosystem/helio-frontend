ef53df6017a702e37be4619e3736b818eba240a6
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-06-05 19:45:58 +0200

    Created a table component

---
+++ src/app/layouts/table/table.component.ts
@@ -0,0 +1,30 @@
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { TableDataSource } from './table-datasource';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<any>;
+  dataSource: TableDataSource;
+
+  @Input() columns;
+  @Input() data;
+
+  ngOnInit() {
+    this.dataSource = new TableDataSource(this.data);
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.table.dataSource = this.dataSource;
+  }
+}
