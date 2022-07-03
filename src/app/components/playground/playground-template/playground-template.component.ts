9ac9abdcfe75c1b831872b4703e34ec61aca091f
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-07-03 21:03:44 +0200

    Modified Playground sections

--- src/app/components/playground/playground-template/playground-template.component.ts
+++ src/app/components/playground/playground-template/playground-template.component.ts
@@ -1,43 +1,77 @@
-import {Component, Input} from '@angular/core';
-import {FormControl} from "@angular/forms";
-import {TranslationService} from "../../../services/translation.service";
-import {TranslationModel} from "../../../models/translation";
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import { FormBuilder, FormControl } from "@angular/forms";
+import { TranslationService } from "../../../services/translation.service";
+import { TranslationModel } from "../../../models/translation";
+import {PlaygroundTourSectionModel} from "../../../models/playground-tour-section";
 
 @Component({
   selector: 'playground-template',
   templateUrl: './playground-template.component.html',
   styleUrls: ['./playground-template.component.css']
 })
-export class PlaygroundTemplateComponent {
+export class PlaygroundTemplateComponent implements OnChanges {
 
-  @Input('') translation;
+  @Input() translation: string;
+  @Input() sectionModel: PlaygroundTourSectionModel;
 
   translationControl: FormControl;
   translationResults: string;
+  translationResultsSuccess: boolean;
+  loadingTranslation: boolean;
 
   private model: TranslationModel =
     new TranslationModel({ id: 'playground', mappingProcessor: '', threads: 1, body: '' });
 
   constructor(
-    private service : TranslationService
-  ) {
-    this.translationControl = new FormControl(this.translation);
+    private fb: FormBuilder,
+    private service : TranslationService) {
+    this.translationControl = this.fb.control(" ");
   }
 
   evaluateTranslation() {
     this.model.setBody(this.translationControl.value);
+    this.loadingTranslation = true;
+    this.translationResults = null;
+    this.translationResultsSuccess = null;
     this.service.add(this.model).subscribe({
       next: (v) => {
         this.translationResults = '';
         this.service.dataValue(this.model.getId()).subscribe({
-          next: (r) => this.translationResults = JSON.stringify(r),
-          error: (errorObtainedResults) => this.translationResults = JSON.stringify(errorObtainedResults)
+          next: (r) => {
+            this.translationResults = r.trim();
+            this.translationResultsSuccess = true;
+            this.loadingTranslation = false;
+          },
+          error: (errorObtainedResults) => {
+            this.translationResults = JSON.stringify(errorObtainedResults)
+            this.translationResultsSuccess = false;
+            this.loadingTranslation = false;
+          }
         });
       },
-      error: (errorInAddTranslation) => this.translationResults = JSON.parse(JSON.stringify(errorInAddTranslation))['error']['message']
+      error: (errorInAddTranslation) => {
+        if (JSON.parse(JSON.stringify(errorInAddTranslation))['error']['message']) {
+          this.translationResults = JSON.parse(JSON.stringify(errorInAddTranslation))['error']['message'];
+        }
+        else {
+          this.translationResults = JSON.parse(JSON.stringify(errorInAddTranslation))['message'];
+        }
+        this.translationResultsSuccess = false;
+        this.loadingTranslation = false;
+      }
     });
+
   }
 
-
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['translation'] || changes['sectionModel']) {
+      if (changes['translation']) {
+        this.translationControl = this.fb.control(changes['translation'].currentValue);
+      }
+      if (changes['sectionModel']) {
+        this.translationControl = this.fb.control(changes['sectionModel'].currentValue._translation);
+      }
+    }
+  }
 
 }
--- src/app/components/playground/playground-tour/playground-tour.component.ts
+++ src/app/components/playground/playground-tour/playground-tour.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {PlaygroundTourService} from "../../../services/playground-tour.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {PlaygroundTourSectionModel} from "../../../models/playground-tour-section";
 
 @Component({
   templateUrl: './playground-tour.component.html',
@@ -6,6 +9,37 @@ import { Component } from '@angular/core';
 })
 export class PlaygroundTourComponent {
 
-  constructor() { }
+  allSectionsEnable: boolean = true;
+  currentSection: PlaygroundTourSectionModel[] = [];
+  previousSection: PlaygroundTourSectionModel = null;
+  nextSection: PlaygroundTourSectionModel = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: PlaygroundTourService)
+  {
+    this.route.params.subscribe(params => {
+      if (params['id'] == null) {
+        this.currentSection = this.service.allSections();
+      }
+      else {
+        const provisionalSection = this.service.section(String(params['id']));
+        if (provisionalSection) {
+          this.currentSection = [provisionalSection];
+          this.previousSection = this.service.previousSection(provisionalSection.sectionId);
+          this.nextSection = this.service.nextSection(provisionalSection.sectionId);
+          this.allSectionsEnable = false;
+        }
+        else {
+            this.router.navigate(['/playground/tour']);
+        }
+      }
+    });
+  }
+
+  public changeSection(sectionId: string) {
+    this.router.navigate(['/playground/tour/' + (sectionId != null ? sectionId : '' )]);
+  }
 
 }
