199ed3635cc8081f63385120932bd93f69c91907
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-07-03 21:04:40 +0200

    Added model & service for playground tour sections

---
+++ src/app/models/playground-tour-section.ts
@@ -0,0 +1,57 @@
+export class PlaygroundTourSectionModel {
+
+    private _sectionId: string;
+    private _name: string;
+    private _description: string;
+    private _translation: string;
+    private _resultTranslation: string;
+
+    constructor({ sectionId, name, description, translation, resultTranslation }) {
+        this._sectionId = sectionId;
+        this._name = name;
+        this._description = description;
+        this._translation = translation;
+        this._resultTranslation = resultTranslation;
+    }
+
+    get sectionId(): string {
+      return this._sectionId;
+    }
+
+    set sectionId(value: string) {
+      this._sectionId = value;
+    }
+
+    get name(): string {
+      return this._name;
+    }
+
+    set name(value: string) {
+      this._name = value;
+    }
+
+    get description(): string {
+      return this._description;
+    }
+
+    set description(value: string) {
+      this._description = value;
+    }
+
+    get translation(): string {
+      return this._translation;
+    }
+
+    set translation(value: string) {
+      this._translation = value;
+    }
+
+    get resultTranslation(): string {
+      return this._resultTranslation;
+    }
+
+    set resultTranslation(value: string) {
+      this._resultTranslation = value;
+    }
+
+}
---
+++ src/app/services/playground-tour.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {PlaygroundTourSectionModel} from "../models/playground-tour-section";
+import allSectionsInJsonFile from '../components/playground/playground-tour/sections.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaygroundTourService {
+
+  private tourSections: PlaygroundTourSectionModel[] = [];
+
+  constructor() {
+    allSectionsInJsonFile.forEach((value, index: number) => this.tourSections.push(new PlaygroundTourSectionModel({
+      sectionId: String(value.name).toLowerCase().replaceAll(' ', '-').replaceAll(/[^a-zA-Z\d-]/g, ''),
+      name: value.name,
+      description: value.description,
+      translation: value.translation_template,
+      resultTranslation: value.expected_results
+    })));
+  }
+
+  allSections(): PlaygroundTourSectionModel[] {
+    return this.tourSections;
+  }
+
+  section(id: string): PlaygroundTourSectionModel {
+    return this.tourSections.find(p => p.sectionId == id);
+  }
+
+  previousSection(currentSectionId: string): PlaygroundTourSectionModel {
+    const index = this.tourSections.findIndex(p => p.sectionId == currentSectionId);
+    return (index - 1 >= 0) ? this.tourSections[index - 1] : null;
+  }
+
+  nextSection(currentSectionId: string): PlaygroundTourSectionModel {
+    const index = this.tourSections.findIndex(p => p.sectionId == currentSectionId);
+    return (index + 1 < this.tourSections.length) ? this.tourSections[index + 1] : null;
+  }
+
+}
