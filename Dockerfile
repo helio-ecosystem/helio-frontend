caea45d22fe16716b0745d5cc28975a4b3381177
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-10-09 17:29:53 +0200

    Updated deployment, Dockerfile & docker-compose for docker environment variables

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,12 @@ ARG profile=production
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
+RUN npm install -g envsub
+
 COPY . .
+
 RUN npm run build -- --output-path=./dist/out --configuration=$profile
+
 
 # Stage 1, based on Nginx, to have only the compiled app, ready for production with Nginx
 FROM nginx:1.22.0-alpine
@@ -15,3 +19,7 @@ COPY --from=build /app/dist/out/ /usr/share/nginx/html
 
 # Copy the default nginx.conf provided by tiangolo/node-frontend
 COPY nginx/nginx.conf /etc/nginx/conf.d/default.conf
+
+# Replace docker env variables in angular app configuration
+CMD ["/bin/sh", "-c", "envsubst < /usr/share/nginx/html/assets/config.template.json> /usr/share/nginx/html/assets/config.json && exec nginx -g 'daemon off;'"]
+
--- deployment.sh
+++ deployment.sh
@@ -4,19 +4,10 @@ read version
 
 echo -e "\nDeployment for production with version: $version"
 
-docker build --no-cache -t emiliocrespoperan/helio-frontend:$version -t emiliocrespoperan/helio-frontend:latest . 
+docker build --force-rm --no-cache -t emiliocrespoperan/helio-frontend:$version -t emiliocrespoperan/helio-frontend:latest .
 
 docker push emiliocrespoperan/helio-frontend:$version
 docker push emiliocrespoperan/helio-frontend:latest
-
-
-echo -e "\nDeployment for playground with version: $version"
-
-docker build --no-cache --build-arg profile=playground -t emiliocrespoperan/helio-frontend-playground:$version -t emiliocrespoperan/helio-frontend-playground:latest . 
-
-docker push emiliocrespoperan/helio-frontend-playground:$version
-docker push emiliocrespoperan/helio-frontend-playground:latest
-
 
 read -rsn1 -p"Press any key to continue";echo
 
